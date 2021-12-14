f2539b7d3a277c5b82fc8a11e3d5dc59d53d3e01
VinayKishore901 <vkishore_be18@thapar.edu>
2021-12-15 00:56:51 +0530

    added team page

--- client/src/MyComponents/Team.js
+++ client/src/MyComponents/Team.js
@@ -5,8 +5,67 @@ export const Team = () => {
         <div>
              <div class="container">
         <div class="section-title">
-            <h1>Our Team Page</h1>
+            <h1>Our Mentors</h1>
         </div>
+        
+
+
+        <div class="row1">
+
+
+<div class="column">
+    <div class="team">
+        <div class="team-img">
+            <img src="img/maam.jpg" alt="Team Image"/>
+        </div>
+
+
+        
+
+
+
+
+        <div class="team-content">
+            <h2>Dr. Nidhi Kalra</h2>
+            <h3>Assistant Professor</h3>
+            <p>Specialized in :Theoretical Computer Science, Smartphone Sensors</p>
+            <h4>nidhi.kalra@thapar.edu</h4>
+            
+        </div>
+         
+    </div>
+</div>
+
+<div class="column">
+    <div class="team">
+        <div class="team-img">
+            <img src="img/sir.jpeg" alt="Team Image"/>
+        </div>
+        <div class="team-content">
+
+        
+
+
+
+
+            <h2>Dr. Anshu Parashar</h2>
+            <h3>Assistant Professor</h3>
+            <p>Specialized in: Software Engineering and ICT for Sustainable Development.</p>
+            <h4>aparashar@thapar.edu</h4>
+        </div>
+       
+    </div>
+</div>
+
+ </div>
+ <div class="container">
+
+ <div class="section-title">
+ <h1>Our Team Page</h1> 
+</div>
+
+
+</div>
 
         <div class="row">
 
@@ -14,19 +73,19 @@ export const Team = () => {
             <div class="column">
                 <div class="team">
                     <div class="team-img">
-                        <img src="img/vinay.png" alt="Team Image"/>
+                        <img src="img/vinay_1.png" alt="Team Image"/>
                     </div>
                     <div class="team-content">
-                        <h2>Steven</h2>
-                        <h3>CEO & Founder</h3>
-                        <p>Some text goes here that describes about team members</p>
-                        <h4>abc@gmail.com</h4>
+                        <h2>VINAY KISHORE</h2>
+                        <h3>101803473 (COE)</h3>
+                        <p>Thapar University</p>
+                        <h4>Upcoming SDE at ION GROUP </h4>
                     </div>
                     <div class="team-social">
                        
-                        <a href="#" class="social-fb"> <i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
-                        <a href="#" class="social-in"> <i class="fab fa-instagram"></i></a>
+                        <a href="vkishore_be18@thapar.edu" class="social-fb"> <i class="fab fa-google"></i></a>
+                        <a href="https://www.linkedin.com/in/vinay-kishore-b865a6185/" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.instagram.com/_vinay.gautam_/" class="social-in"> <i class="fab fa-instagram"></i></a>
                         
                     </div>
                 </div>
@@ -35,19 +94,30 @@ export const Team = () => {
             <div class="column">
                 <div class="team">
                     <div class="team-img">
-                        <img src="img/team-2.jpg" alt="Team Image"/>
+                        <img src="img/kush.png" alt="Team Image"/>
                     </div>
                     <div class="team-content">
-                        <h2>Mayra</h2>
-                        <h3>Art Director</h3>
-                        <p>Some text goes here that describes about team members</p>
-                        <h4>abc@gmail.com</h4>
+
+
+
+                  
+
+
+
+
+
+
+
+                        <h2>  KUSH GUPTA</h2>
+                        <h3>101803454 (COE)</h3>
+                        <p>Thapar University</p>
+                        <h4>Upcoming ANALYST at ESTEE ADVISORS  </h4>
                     </div>
                     <div class="team-social">
                         
-                        <a href="#" class="social-fb"> <i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
-                        <a href="#" class="social-in"> <i class="fab fa-instagram"></i></a>
+                        <a href="kgupta1_be18@thapar.edu" class="social-fb"> <i class="fab fa-google"></i></a>
+                        <a href="https://www.linkedin.com/in/kush-gupta-17ba1819b/" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.instagram.com/kushhhh.here_/" class="social-in"> <i class="fab fa-instagram"></i></a>
                         
                     </div>
                 </div>
@@ -56,19 +126,29 @@ export const Team = () => {
             <div class="column">
                 <div class="team">
                     <div class="team-img">
-                        <img src="img/team-3.jpg" alt="Team Image"/>
+                        <img src="img/piyush.jpeg" alt="Team Image"/>
                     </div>
                     <div class="team-content">
-                        <h2>John</h2>
-                        <h3>Developer</h3>
-                        <p>Some text goes here that describes about team members</p>
-                        <h4>abc@gmail.com</h4>
+
+                   
+
+
+
+
+
+
+
+
+                        <h2>PIYUSH SETHI</h2>
+                        <h3>101803471 (COE)</h3>
+                        <p>Thapar University</p>
+                        <h4>Upcoming SDE at AMADEUS</h4>
                     </div>
                     <div class="team-social">
                        
-                        <a href="#" class="social-fb"> <i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
-                        <a href="#" class="social-in"> <i class="fab fa-instagram"></i></a>
+                        <a href="psethi_be18@thapar.edu" class="social-fb"> <i class="fab fa-google"></i></a>
+                        <a href="https://www.linkedin.com/in/piyush-sethi-1a1181171/" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.instagram.com/p_sethi_/" class="social-in"> <i class="fab fa-instagram"></i></a>
                 
                     </div>
                 </div>
@@ -78,27 +158,36 @@ export const Team = () => {
                 <div class="team">
                     
                     <div class="team-img">
-                        <img src="img/team-4.jpg" alt="Team Image"/>
+                        <img src="img/kritika.jpg" alt="Team Image"/>
                     </div>
                     <div class="team-content">
-                        <h2>Jennifer</h2>
-                        <h3>Designer</h3>
-                        <p>Some text goes here that describes about team members</p>
-                        <h4>abc@gmail.com</h4>
+
+                    
+
+
+
+
+
+
+                        <h2>KRITIKA</h2>
+                        <h3>101803155 (COE)</h3>
+                        <p>Thapar University</p>
+                        <h4>Upcoming SDE at PROVIDENCE </h4>
                     </div>
                     <div class="team-social">
                          
-                        <a href="#" class="social-fb"> <i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
-                        <a href="#" class="social-in"> <i class="fab fa-instagram"></i></a>
+                        <a href="kkritika_be18@thapar.edu" class="social-fb"> <i class="fab fa-facebook-f"></i></a>
+                        <a href="http://www.linkedin.com/in/kj19" class="social-li"> <i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.instagram.com/19jen.ny/" class="social-in"> <i class="fab fa-instagram"></i></a>
                        
                     </div>
                 </div>
             </div>
 
         </div>
-
+        <br/> <br/> <br/>
     </div>
         </div>
+       
     )
 }
